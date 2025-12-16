28960cc6f4a94d75ce75549764d567c1438cec44
NotAbdieZ <146970580+NotAbdiez@users.noreply.github.com>
2025-12-16 10:37:08 +0100

    Refactor VerifyAccess function and event registrations

--- src/client/client.lua
+++ src/client/client.lua
@@ -10,22 +10,8 @@ RegisterNetEvent("txcl:spectate:start", function()
     VerifyAccess()
 end)
 
-RegisterNetEvent("txcl:heal", function()
-    VerifyAccess()
-end)
-
-RegisterNetEvent("txcl:tpToWaypoint", function()
-    VerifyAccess()
-end)
-
 RegisterNetEvent("txcl:showPlayerIDs", function()
     VerifyAccess()
 end)
 
-RegisterNetEvent("txcl:showAnnouncement", function()
-    VerifyAccess()
-end)
 
-RegisterNetEvent("txcl:vehicle:fix", function()
-    VerifyAccess()
-end)
